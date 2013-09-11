20ec2a986b6c60381083eb93b887c823d1f85555
edymerchk <edyhack@gmail.com>
2013-09-11 09:00:45 -0500

    adding some tests

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,18 @@
+require 'calculator'
+
+describe Calculator do
+
+  it "adds two numbers" do
+
+  end
+  it "adds four numbers" do
+
+  end
+  it "divides two positive numbers" do
+
+  end
+  it "raise an error when divides by 0" do
+
+  end
+end
+
